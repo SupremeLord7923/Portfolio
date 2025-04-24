37c8187b752b828e6316a39617fe5d1283d1a23d
SupremeLord7923 <maliknjorogemurithi@gmail.com>
2025-04-24 13:42:44 +0300

    final edition

---
+++ index.py
@@ -0,0 +1,18 @@
+# My Pet Robot 🧠🤖
+print("Hello! I am Mavis.")
+name = input("What is your name? ")
+print("Nice to meet you, " + name + "!")
+
+age = input("How old are you?")
+print("Interesting")
+
+place = input("Where do you live?")
+print("Wow! That is nice")
+
+color = input("What is your favorite color? ")
+print("Wow! I love the color " + color + " too!")
+
+animal = input("What is your favorite animal? ")
+print("Cool! I wish I could have a pet " + animal + ".")
+
+print("Thanks for chatting with me, " + name + "! Bye 👋")
